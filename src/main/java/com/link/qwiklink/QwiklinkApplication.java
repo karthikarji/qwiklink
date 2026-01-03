a47bb8307001fef7bed3f96154cea77cc506f390
karthikarji <ak28@iitbbs.ac.in>
2026-01-03 02:27:48 +0100

    Added Postgres Configuration

---
+++ src/main/java/com/link/qwiklink/QwiklinkApplication.java
@@ -0,0 +1,13 @@
+package com.link.qwiklink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QwiklinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QwiklinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/link/qwiklink/QwiklinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.link.qwiklink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QwiklinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
